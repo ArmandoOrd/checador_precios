575b7ea2e3c0c5d737e7b9c20830dc8898d40f1d
Armando <j301297@gmail.com>
2019-09-20 17:37:34 -0500

    primer entregable al dia 23/09/2019

---
+++ checadorPrecios/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace checadorPrecios.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
